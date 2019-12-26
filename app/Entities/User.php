1b728308081df465c57db370ca5fe37aa230aa3d
FranciscoACLima <franciscoaclima@outlook.com>
2019-12-26 16:45:00 -0300

    Criação dos models para as tabelas users e  users_socials

---
+++ app/Entities/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Entities;
+
+//use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Elequent\SoftDeletes;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+    use SoftDeletes;
+
+    protected $table = 'users';
+
+    /**
+     * Os atributos que são atribuíveis em massa
+     * 
+     * Na criação do objeto passar estes atributos na forma
+     * de lista
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'cpf', 'name', 'phone', 'birth', 'gender',
+        'notes', 'email', 'password',
+    ];
+
+    /**
+     * Atributos ocultos nos arrays de resultado das 
+     * buscas no banco de dados
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+   /*  protected $casts = [
+        'email_verified_at' => 'datetime',
+    ]; 
+    */
+} 
---
+++ database/migrations/old/2019_12_26_183919_create_users_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateUsersTable.
+ */
+class CreateUsersTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table) {
+			$table->increments('id');
+
+			// dados pessoais
+			$table->string('cpf', 11)->unique()->nullable();
+			$table->string('name', 50);
+			$table->char('phone', 11);
+			$table->date('bird')->nullable();
+			$table->char('gender', 1)->nullable();
+			$table->text('notes')->ullable();
+
+			// dados de autenticação
+			$table->string('email', 80)->unique();
+			$table->string('password', 254)->nullable();
+
+			// permissões
+			$table->string('status')->default('active');
+			$table->string('permission')->default('app.user');
+
+			$table->rememberToken();
+
+			$table->timestamps(); // created_at, updated_at, deleted_at
+			$table->softDeletes();  // cria um sistema de exclusão falso
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+}
